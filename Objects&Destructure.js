250890eb5b804e49afbac1e7708979d24ed0deb1
Chanakya-dev <Chanakyamanas123@gmail.com>
2025-01-04 14:39:57 +0530

    Added Objects Data

--- Objects&Destructure.js
+++ Objects&Destructure.js
@@ -1,57 +1,3 @@
-### Objects and Maps
-```js
-const users = [
-    { id: 1, name: 'John', age: 25, city: 'New York', job: 'Developer' },
-    { id: 2, name: 'Alice', age: 30, city: 'Los Angeles', job: 'Designer' },
-    { id: 3, name: 'Bob', age: 22, city: 'Chicago', job: 'Artist' },
-    { id: 4, name: 'Emily', age: 27, city: 'San Francisco', job: 'Engineer' }
-  ];
-  
-  const userDescriptions = users.map(({ name, city, job }) => {
-    return `${name}, a ${job}, lives in ${city}.`;
-  });
-  console.log(userDescriptions); 
-  
-  
-  const usersWithFullInfo = users.map(user => {
-    const fullInfo = `${user.name}, aged ${user.age}, lives in ${user.city} and works as a(n) ${user.job}.`;
-    return { ...user, fullInfo };
-  });
-  console.log(usersWithFullInfo);
-  
-
-  
-  
-  const jobTitles = users.map(user => user.job);
-  console.log(jobTitles);
-  
-  const transformedUsers = users.map(({ name, city, job }) => {
-    return {
-      userName: name.toUpperCase(),
-      userCity: city.toLowerCase(),
-      userJob: job.charAt(0).toUpperCase() + job.slice(1)
-    };
-  });
-  
-  console.log(transformedUsers);
-  const usersWithExperience = users.map(user => {
-    const experience = user.age - 18;
-    return { ...user, experience };
-  });
-  console.log(usersWithExperience);
-  
-  const usersWithStatus = usersWithExperience.map(user => {
-    const status = user.experience > 7 ? 'Experienced' : 'Junior';
-    return { ...user, status };
-  });
-  console.log(usersWithStatus);
-  
-  ```
-
-### Objects and Destructure
-
-
-```js
 const car = {
     make: 'Toyota',
     model: 'Corolla',
@@ -175,43 +121,6 @@ const car = {
   const { name: copiedName, occupation: copiedOccupation } = copy;
   console.log(copiedName);
   console.log(copiedOccupation);
-```
-### Object (Multi Attribute) Destructure
+  
 
-````js
-const complexObject = {
-    fname: 'Alice',
-    age: 30,
-    isActive: true,
-    hobbies: ['Reading', 'Hiking', 'Coding'],
-    address: {
-      street: '123 Main St',
-      city: 'New York',
-      zipCode: '10001'
-    },
-    greet: function() {
-      return `Hello, my name is ${this.fname}.`;
-    },
-  };
-  
-  const {
-    fname,
-    age,
-    isActive,
-    hobbies,
-    address: { street, city, zipCode },
-    greet
-  } = complexObject;
-  
-  const boundGreet = greet.bind(complexObject);
-  
-  console.log(fname);        // Alice
-  console.log(age);         // 30
-  console.log(isActive);    // true
-  console.log(hobbies);     // ['Reading', 'Hiking', 'Coding']
-  console.log(street);      // 123 Main St
-  console.log(city);        // New York
-  console.log(zipCode);     // 10001
-  console.log(boundGreet()); // Hello, my name is Alice.
-  
-  ````
+
